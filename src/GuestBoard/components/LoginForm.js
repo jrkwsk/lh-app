1b61081c87d7ee670d98dde9a03f793f8d3cc098
jrkwsk <ula.jurkowska@gmail.com>
2021-06-05 13:14:13 +0200

    redirect to main dashboard

--- src/GuestBoard/components/LoginForm.js
+++ src/GuestBoard/components/LoginForm.js
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUser } from '../../store/authSlice'
+import {Redirect} from "react-router-dom";
 
 
 export const LoginForm = () => {
@@ -15,83 +16,80 @@ export const LoginForm = () => {
     } = useForm({
         mode: 'onSubmit',
         reValidateMode: 'onChange',
-        defaultValues: {email: undefined, password: undefined},
+        defaultValues: { email: undefined, password: undefined },
 
-      })
-   
+    })
+
     const handleOnsubmit = (data) => {
         dispatch(fetchUser(data))
     }
 
-        return (
+
+
+    return (
         <div className="container">
             <div className="row">
-{auth.isAuthenticated === false &&
+                {auth.isAuthenticated === false &&
 
 
-                <div className="col-8">
+                    <div className="col-8">
 
-                    <h1 className="display-3 m-5">Hello, login here!</h1>
-                  
+                        <h1 className="display-3 m-5">Hello, login here!</h1>
 
-                    <form onSubmit={handleSubmit(handleOnsubmit)}>
 
-                       
-                        <div className="form-group row">
-                            <label htmlFor="" className="col-2 col-form-label">Email</label>
-                            <div className="col-8">
-                                <input
-                                    type="text" className="form-control" id="" placeholder="Email"
-                                    {...register("email", {
-                                        required: true
-                                    })}
-                                ></input>
-                                {errors.email && <p>this is required</p>}
+                        <form onSubmit={handleSubmit(handleOnsubmit)}>
 
+
+                            <div className="form-group row">
+                                <label htmlFor="" className="col-2 col-form-label">Email</label>
+                                <div className="col-8">
+                                    <input
+                                        type="text" className="form-control" id="" placeholder="Email"
+                                        {...register("email", {
+                                            required: true
+                                        })}
+                                    ></input>
+                                    {errors.email && <p>this is required</p>}
+
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-group row">
+                            <div className="form-group row">
 
-                            <label htmlFor="" className="col-2 col-form-label">Password</label>
-                            <div className="col-8">
-                                <input type="password" className="form-control" id="" placeholder="Password"
-                                    {...register("password", {
-                                        required: true
-                                    })}
-                                ></input>
-                                {errors.password && <p>this is required</p>}
+                                <label htmlFor="" className="col-2 col-form-label">Password</label>
+                                <div className="col-8">
+                                    <input type="password" className="form-control" id="" placeholder="Password"
+                                        {...register("password", {
+                                            required: true
+                                        })}
+                                    ></input>
+                                    {errors.password && <p>this is required</p>}
 
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="form-group row">
-                            <div className="col-8">
-                                <button type="submit" className="btn btn-primary">Sign up</button>
+                            <div className="form-group row">
+                                <div className="col-8">
+                                    <button type="submit" className="btn btn-primary">Sign up</button>
+                                </div>
                             </div>
-                        </div>
 
 
-                    </form>
-                </div>
-}
-{auth.isAuthenticated === true &&
+                        </form>
+                    </div>
+                }
+                {auth.isAuthenticated === true &&
 
-                 <div>
-                 <div className="alert alert-success" role="alert">
-    <strong>Well done!</strong> You successfully logged in!</div>    
+<Redirect to="/MainUserView" />
+                }
+
+                {(auth.isAuthenticated === false && auth.status === "rejected") &&
+                    <div>
+                        <div className="alert alert-success" role="alert">
+                            <strong>Oops!</strong> Try again!</div>
                     </div>
                 }
 
-{(auth.isAuthenticated === false && auth.status==="rejected") && 
-<div>
-                 <div className="alert alert-success" role="alert">
-    <strong>Oops!</strong> Try again!</div>    
-                    </div>
-                }
-
-
-
-        </div>
+            </div>
         </div>
     )
 
